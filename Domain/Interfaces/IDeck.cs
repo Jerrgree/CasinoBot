3ecdb25a69b93966ce22da4409edd44a865491ed
Jerrgree <jeremyrgreenburg@gmail.com>
2022-01-13 12:03:30 -0600

    Add basic deck interface

---
+++ Domain/Interfaces/IDeck.cs
@@ -0,0 +1,36 @@
+﻿namespace Domain.Interfaces
+{
+    public interface IDeck<T> where T : ICard
+    {
+        /// <summary>
+        /// Returns the remaining number cards in the deck
+        /// </summary>
+        public int Count { get; }
+
+        /// <summary>
+        /// Shuffles the remaining cards in the deck
+        /// </summary>
+        public void Shuffle();
+
+        /// <summary>
+        /// Draws a single card from the top of the deck
+        /// </summary>
+        /// <returns></returns>
+        public T Draw();
+
+        /// <summary>
+        /// Draws multiple cards from the top of the deck
+        /// </summary>
+        /// <param name="numberOfCards">The number of cards to draw</param>
+        /// <returns></returns>
+        public IEnumerable<T> Draw(int numberOfCards);
+
+        /// <summary>
+        /// Creates hand for the number of specified players, given the number of cards provided
+        /// </summary>
+        /// <param name="numberOfPlayers">The number </param>
+        /// <param name="numberOfCards"></param>
+        /// <returns></returns>
+        public IEnumerable<IEnumerable<T>> Deal(int numberOfPlayers, int numberOfCards);
+    }
+}
