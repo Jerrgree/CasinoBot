3810ff47296b501ac6eaa9639af234df577b81ac
Jerrgree <jeremyrgreenburg@gmail.com>
2022-01-13 22:05:06 -0600

    WIP Game business logic

---
+++ Games.BlackJack/Domain/Models/Player.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Interfaces;
+using Domain.Models.StandardPlayingCards;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Games.BlackJack.Domain.Models
+{
+    internal class Player
+    {
+        internal IHand<StandardPlayingCard> Hand { get; }
+    }
+}
