f9942c038adb44c82b590db2ed70702ed5d96857
Jeremy Greenburg <jeremyrgreenburg@gmail.com>
2022-01-13 18:08:31 -0600

    Add Hands (#5)
    Add hands

---
+++ Domain/Interfaces/IHand.cs
@@ -0,0 +1,48 @@
+﻿namespace Domain.Interfaces
+{
+    public interface IHand<T> where T : ICard
+    {
+        /// <summary>
+        /// Retrieves the number of cards in this hand
+        /// </summary>
+        int Count { get; }
+
+        /// <summary>
+        /// Adds a new card to the hand, hidden from the table
+        /// </summary>
+        /// <param name="card">The new card to be added</param>
+        void AddCard(T card);
+
+        /// <summary>
+        /// Adds a new card to the hand
+        /// </summary>
+        /// <param name="card">The new card to be added</param>
+        /// <param name="isPublic">Indicates if this card is visible to the table. </param>
+        void AddCard(T card, bool isPublic);
+
+        /// <summary>
+        /// Accesses the indicated card
+        /// </summary>
+        /// <param name="index">The index of the card to retrieve</param>
+        /// <exception cref="IndexOutOfRangeException">Thrown when attempting to access an index outside of the hand's range</exception>
+        /// <returns>The specified card, along with it's access level</returns>
+        (T Card, bool isPublic) this[int index]
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Removes the given card from the hand. 
+        /// </summary>
+        /// <param name="index">The index of the card to remove</param>
+        /// <exception cref="IndexOutOfRangeException">Thrown when attempting to access an index outside of the hand's range</exception>
+        /// <returns>The removed card</returns>
+        T RemoveAt(int index);
+
+        /// <summary>
+        /// Randomizes the hand
+        /// </summary>
+        /// <exception cref="InvalidOperationException">Thrown when called on an empty hand</exception>
+        void Shuffle();
+    }
+}
---
+++ Domain/Models/Hands/Hand.cs
@@ -0,0 +1,45 @@
+﻿using Common.Extensions;
+using Domain.Interfaces;
+
+namespace Domain.Models.Hands
+{
+    public class Hand<T> : IHand<T> where T : ICard
+    {
+        private List<(T Card, bool isPublic)> _cards;
+
+        public int Count => _cards.Count;
+
+        public Hand()
+        {
+            _cards = new List<(T, bool)>();
+        }
+
+        public (T Card, bool isPublic) this[int index] 
+        { 
+            get => _cards[index];
+        }
+
+        public void AddCard(T card)
+        {
+            AddCard(card, false);
+        }
+
+        public void AddCard(T card, bool isPublic)
+        {
+            _cards.Add((card, isPublic));
+        }
+
+        public T RemoveAt(int index)
+        {
+            var card = _cards[index].Card;
+            _cards.RemoveAt(index);
+            return card;
+        }
+
+        public void Shuffle()
+        {
+            if (Count == 0) throw new InvalidOperationException("The hand is currently empty");
+            _cards.Shuffle();
+        }
+    }
+}
