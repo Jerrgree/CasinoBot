06f96c288f6443b2e38a49d43422ed146650d6b0
Jerrgree <jeremyrgreenburg@gmail.com>
2022-01-14 21:03:06 -0600

    Add start of Discord Integration

---
+++ CasinoBot.Interaction.Discord.Client/DiscordClient.cs
@@ -0,0 +1,41 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace CasinoBot.Interaction.Discord.Client
+{
+    public class DiscordClient : IDisposable
+    {
+        private readonly string _token;
+        private DiscordSocketClient _client;
+
+        public DiscordClient(string token)
+        {
+            if (string.IsNullOrWhiteSpace(token)) throw new ArgumentNullException(nameof(token));
+            _token = token;
+
+            _client = new DiscordSocketClient();
+        }
+
+        public async Task Connect()
+        {
+            await _client.LoginAsync(TokenType.Bot, _token);
+        }
+
+        public async Task Start()
+        {
+            if (_client.Status == UserStatus.Offline) throw new InvalidOperationException("Bot is not online");
+            await _client.StartAsync();
+        }
+
+        public async Task Stop()
+        {
+            await _client.StopAsync();
+        }
+
+        public void Dispose()
+        {
+            _client?.Dispose();
+        }
+
+    }
+}
---
+++ CasinoBot.Interaction.Discord/Program.cs
@@ -0,0 +1,33 @@
+﻿using CasinoBot.Interaction.Discord.Client;
+using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("discordSettings.json").Build();
+
+var discordSettings = config.GetSection("DiscordConfig");
+
+var discordClient = new DiscordClient(discordSettings["token"]);
+
+try
+{
+    await discordClient.Connect();
+
+    await discordClient.Start();
+
+    var endTime = DateTime.UtcNow.AddMinutes(2);
+
+    do
+    {
+        await Task.Delay(1000);
+    } while (DateTime.UtcNow < endTime);
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Exception Encountered: {ex}");
+}
+finally
+{
+    await discordClient.Stop();
+}
