5f734c114eb296dda2857654c2b7021b30531883
Jerrgree <jeremyrgreenburg@gmail.com>
2022-01-15 09:43:05 -0600

    Progress on fancy messages

--- CasinoBot.Interaction.Discord.Client/Modules/DrawCardModule.cs
+++ CasinoBot.Interaction.Discord.Client/Modules/DrawCardModule.cs
@@ -1,4 +1,5 @@
 ﻿using CasinoBot.Domain.Models.StandardPlayingCards;
+using Discord;
 using Discord.Interactions;
 
 namespace CasinoBot.Interaction.Discord.Client.Modules
@@ -9,8 +10,11 @@ namespace CasinoBot.Interaction.Discord.Client.Modules
         public async Task Echo([Summary(description: "The number of cards to draw"), MinValue(1)] int numberOfCards = 1)
         {
             var cards = StandardPlayingCard.CreateDeck(1).Shuffle().Draw(numberOfCards);
-            var result = $"Draw {string.Join(", ", cards.Select(c => c.Display))}";
-            await RespondAsync(result);
+
+            var embed = new EmbedBuilder()
+                .WithTitle("Cards")
+                .WithDescription(string.Join(" ", cards.Select(c => ":eight: of :heart:")));
+            await RespondAsync(embed: embed.Build());
         }
     }
 }
