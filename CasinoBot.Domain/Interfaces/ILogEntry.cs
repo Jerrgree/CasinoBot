e942417291802597cbe9fb3da6767c536a02a723
Jerrgree <jeremyrgreenburg@gmail.com>
2022-01-15 20:56:34 -0600

    Add interfaces

---
+++ CasinoBot.Domain/Interfaces/ILogEntry.cs
@@ -0,0 +1,9 @@
+﻿namespace CasinoBot.Domain.Interfaces
+{
+    public interface ILogEntry
+    {
+        public string Message { get; set; }
+        public ulong? UserId { get; set; }
+        public Guid? TraceId { get; set; }
+    }
+}
---
+++ CasinoBot.Domain/Interfaces/ILoggingService.cs
@@ -0,0 +1,11 @@
+﻿namespace CasinoBot.Domain.Interfaces
+{
+    public interface ILoggingService
+    {
+        Task LogDebugMessage(ILogEntry logEntry);
+        Task LogInformationalMessage(ILogEntry logEntry);
+        Task LogWarningMessage(ILogEntry logEntry);
+        Task LogErrorMessage(ILogEntry logEntry);
+        Task LogFatalMessage(ILogEntry logEntry);
+    }
+}
