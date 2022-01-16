9fc3c8b881d41f3db932030035c1b22ce168632b
Jeremy Greenburg <jeremyrgreenburg@gmail.com>
2022-01-15 21:08:17 -0600

    Add Logging Service (#20)
    * Add logging interfaces
    
    * Add console logging service

---
+++ CasinoBot.Domain/Interfaces/ILogEntry.cs
@@ -0,0 +1,18 @@
+﻿namespace CasinoBot.Domain.Interfaces
+{
+    public interface ILogEntry
+    {
+        /// <summary>
+        /// The message to write to the logs
+        /// </summary>
+        public string Message { get; set; }
+        /// <summary>
+        /// The ID of the user associated with this log
+        /// </summary>
+        public ulong? UserId { get; set; }
+        /// <summary>
+        /// The trace ID of the request associated with this log
+        /// </summary>
+        public Guid? TraceId { get; set; }
+    }
+}
---
+++ CasinoBot.Logging.ConsoleLogger/ConsoleLogger.cs
@@ -0,0 +1,36 @@
+﻿using CasinoBot.Domain.Interfaces;
+
+namespace CasinoBot.Logging.ConsoleLogger
+{
+    public class ConsoleLogger : ILoggingService
+    {
+        public Task LogDebugMessage(ILogEntry logEntry)
+        {
+            Console.WriteLine($"Debug:\tUser: {logEntry.UserId}\tTrace: {logEntry.TraceId}\tTime: {DateTime.Now.ToLongDateString()}\n{logEntry.Message}");
+            return Task.CompletedTask;
+        }
+        public Task LogErrorMessage(ILogEntry logEntry)
+        {
+            Console.WriteLine($"Error:\tUser: {logEntry.UserId}\tTrace: {logEntry.TraceId}\tTime: {DateTime.Now.ToLongDateString()}\n{logEntry.Message}");
+            return Task.CompletedTask;
+        }
+
+        public Task LogFatalMessage(ILogEntry logEntry)
+        {
+            Console.WriteLine($"Fatal:\tUser: {logEntry.UserId}\tTrace: {logEntry.TraceId}\tTime: {DateTime.Now.ToLongDateString()}\n{logEntry.Message}");
+            return Task.CompletedTask;
+        }
+
+        public Task LogInformationalMessage(ILogEntry logEntry)
+        {
+            Console.WriteLine($"Information:\tUser: {logEntry.UserId}\tTrace: {logEntry.TraceId}\tTime: {DateTime.Now.ToLongDateString()}\n{logEntry.Message}");
+            return Task.CompletedTask;
+        }
+
+        public Task LogWarningMessage(ILogEntry logEntry)
+        {
+            Console.WriteLine($"Warning:\tUser: {logEntry.UserId}\tTrace: {logEntry.TraceId}\tTime: {DateTime.Now.ToLongDateString()}\n{logEntry.Message}");
+            return Task.CompletedTask;
+        }
+    }
+}
