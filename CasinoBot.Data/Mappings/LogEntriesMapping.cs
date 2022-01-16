0ad28e8f16e836976ebfcfdf88cea60c752d766b
Jeremy Greenburg <jeremyrgreenburg@gmail.com>
2022-01-16 16:15:22 -0600

    Add Database (#24)

---
+++ CasinoBot.Data/Mappings/LogEntriesMapping.cs
@@ -0,0 +1,15 @@
+﻿using CasinoBot.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CasinoBot.Data.Mappings
+{
+    internal class LogEntriesMapping : IEntityTypeConfiguration<LogEntry>
+    {
+        public void Configure(EntityTypeBuilder<LogEntry> builder)
+        {
+            builder
+                .HasKey(l => l.LogEntryId);
+        }
+    }
+}
---
+++ CasinoBot.Domain/Enums/LogLevel.cs
@@ -0,0 +1,11 @@
+﻿namespace CasinoBot.Domain.Enums
+{
+    public enum LogLevel : byte
+    {
+        Debug = 0,
+        Information = 1,
+        Warning = 2,
+        Error = 3,
+        Fatal = 4
+    }
+}
