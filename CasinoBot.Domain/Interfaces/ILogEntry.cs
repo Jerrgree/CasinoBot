9aa948dcf347313c826ccec527df8eb7d242884a
Jerrgree <jeremyrgreenburg@gmail.com>
2022-01-15 21:08:02 -0600

    Add interface comments

--- CasinoBot.Domain/Interfaces/ILogEntry.cs
+++ CasinoBot.Domain/Interfaces/ILogEntry.cs
@@ -2,8 +2,17 @@
 {
     public interface ILogEntry
     {
+        /// <summary>
+        /// The message to write to the logs
+        /// </summary>
         public string Message { get; set; }
+        /// <summary>
+        /// The ID of the user associated with this log
+        /// </summary>
         public ulong? UserId { get; set; }
+        /// <summary>
+        /// The trace ID of the request associated with this log
+        /// </summary>
         public Guid? TraceId { get; set; }
     }
 }
