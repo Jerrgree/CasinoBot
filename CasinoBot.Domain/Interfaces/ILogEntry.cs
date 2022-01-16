e7bcdb2a9542349d4edb32ace83baeb87e769655
Jeremy Greenburg <jeremyrgreenburg@gmail.com>
2022-01-15 21:31:10 -0600

    Integrate logging service to Discord Client (#21)

--- CasinoBot.Domain/Interfaces/ILogEntry.cs
+++ CasinoBot.Domain/Interfaces/ILogEntry.cs
@@ -5,14 +5,14 @@
         /// <summary>
         /// The message to write to the logs
         /// </summary>
-        public string Message { get; set; }
+        public string Message { get; }
         /// <summary>
         /// The ID of the user associated with this log
         /// </summary>
-        public ulong? UserId { get; set; }
+        public ulong? UserId { get; }
         /// <summary>
         /// The trace ID of the request associated with this log
         /// </summary>
-        public Guid? TraceId { get; set; }
+        public Guid? TraceId { get; }
     }
 }
