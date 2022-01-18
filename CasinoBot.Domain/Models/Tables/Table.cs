42092a806e6bde808007856b7fc1b5b750ace50a
Jerrgree <jeremyrgreenburg@gmail.com>
2022-01-17 22:05:44 -0600

    Update table model

---
+++ CasinoBot.Domain/Models/Tables/Table.cs
@@ -0,0 +1,12 @@
+﻿using CasinoBot.Domain.Enums;
+
+namespace CasinoBot.Domain.Models.Tables
+{
+    public class Table
+    {
+        public ulong GuildId {get;set;}
+        public TableType TableType { get; set; }
+
+        public List<object> Users { get; set; }
+    }
+}
