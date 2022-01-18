203f56156a52a29fad5205ebebb9485cbe935aff
Jerrgree <jeremyrgreenburg@gmail.com>
2022-01-17 22:10:33 -0600

    Add player model

---
+++ CasinoBot.Domain/Models/Players/Player.cs
@@ -0,0 +1,9 @@
+﻿namespace CasinoBot.Domain.Models.Players
+{
+    public class Player<T> where T : class
+    {
+        public ulong PlayerId { get; set; }
+
+        public T State { get; set; }
+    }
+}
