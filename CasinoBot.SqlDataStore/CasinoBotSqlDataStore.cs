b55be778eb689b408d3e739b04876cc193c446e7
Jerrgree <jeremyrgreenburg@gmail.com>
2022-01-22 22:11:03 -0600

    Stubbing out methods

--- CasinoBot.SqlDataStore/CasinoBotSqlDataStore.cs
+++ CasinoBot.SqlDataStore/CasinoBotSqlDataStore.cs
@@ -1,7 +1,8 @@
 ﻿using CasinoBot.Data;
+using CasinoBot.Data.Entities;
 using CasinoBot.Domain.Enums;
 using CasinoBot.Domain.Interfaces;
-using CasinoBot.SqlDataStore.Models;
+using Newtonsoft.Json;
 using System.Runtime.CompilerServices;
 
 namespace CasinoBot.SqlDataStore
@@ -16,44 +17,71 @@ namespace CasinoBot.SqlDataStore
             _loggingService = loggingService;
             _dbContext = dbContext;
         }
-        public Task<(bool isSuccessful, string message)> AddPlayerToTable<T>(long tableId, ulong playerId, T playerState) where T : class
+
+        public async Task<(bool isSuccessful, string message)> CreateTable(ulong guildId, TableType tableType)
         {
             throw new NotImplementedException();
         }
 
-        public Task<(bool isSuccessful, string message)> CreateTable(ulong guildId, TableType tableType)
+        public async Task<(bool isSuccessful, string message)> DeleteTable(long tableId)
         {
             throw new NotImplementedException();
         }
 
-        public Task<(bool isSuccessful, string message)> DeleteTable(long tableId)
+        public async Task<(bool isSuccessful, IEnumerable<ITable<T>> tables, string message)> GetTablesByGuild<T>(ulong guildId) where T : class
         {
             throw new NotImplementedException();
         }
 
-        public Task<(bool isSuccessful, IEnumerable<ITable<T>> tables, string message)> GetTablesByGuild<T>(ulong guildId) where T : class
+        public async Task<(bool isSuccessful, string message, IEnumerable<ITable<T>> tables)> GetTablesByPlayer<T>(ulong playerId) where T : class
         {
             throw new NotImplementedException();
         }
 
-        public Task<(bool isSuccessful, string message, IEnumerable<ITable<T>> tables)> GetTablesByPlayer<T>(ulong playerId) where T : class
+        public async Task<(bool isSuccessful, string message)> AddPlayerToTable<T>(long tableId, ulong playerId, T playerState) where T : class
         {
-            throw new NotImplementedException();
-        }
-
-        public Task<(bool isSuccessful, string message)> UpdatePlayer<T>(long tableId, ulong playerId, T playerState) where T : class
-        {
-            throw new NotImplementedException();
-        }
-
-        private string HandleException(Exception ex, ulong? userId, [CallerMemberName] string callerName = "")
-        {
-            var logMessage = new LogEntry($"Exception encountered in {callerName}. Ex = {ex}")
+            try
             {
-                UserId = userId
-            };
-            _loggingService.LogErrorMessage(logMessage)
-            throw new Exception("TODO");
+                var userTable = new UserTable()
+                {
+                    TableId = tableId,
+                    UserId = playerId,
+                    State = JsonConvert.SerializeObject(playerState)
+                };
+
+                _dbContext.UserTables.Add(userTable);
+
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                var message = HandleException(ex); // Maybe return an enum instead?
+                return (false, message);
+            }
+
+            return (true, null);
+        }
+
+
+        public async Task<(bool isSuccessful, string message)> UpdatePlayer<T>(long tableId, ulong playerId, T playerState) where T : class
+        {
+            try
+            {
+                var playerTable = _dbContext.UserTables.First(ut => ut.UserId == playerId && ut.TableId == tableId);
+            }
+            catch (Exception ex)
+            {
+                var message = HandleException(ex); // Maybe return an enum instead?
+                return (false, message);
+            }
+
+            return (true, null);
+        }
+
+        private string HandleException(Exception ex, [CallerMemberName] string callerName = "")
+        {
+            _loggingService.LogErrorMessage($"Exception encountered in {callerName}. Ex = {ex}");
+            return "TODO";
         }
     }
 }
