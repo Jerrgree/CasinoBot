3715799212f64a00d147cf7f16a67d96d20ae7b2
Jerrgree <jeremyrgreenburg@gmail.com>
2022-01-25 23:19:30 -0600

    Integrating buttons

--- CasinoBot.Interaction.Discord.Client/Modules/TableModule.cs
+++ CasinoBot.Interaction.Discord.Client/Modules/TableModule.cs
@@ -9,11 +9,16 @@ namespace CasinoBot.Interaction.Discord.Client.Modules
     public class TableModule : CasinoBotInteractionModuleBase
     {
         private readonly IGameDataStore _gameDataStore;
+        private static Emoji _nextButton = new Emoji("\U000025B6");
+        private static Emoji _prevButton = new Emoji("\U000025C0");
         public TableModule(ILoggingService loggingService,
             IGameDataStore gameDataStore) : base(loggingService) 
         {
             _gameDataStore = gameDataStore;
         }
+        
+
+        #region Slash Commands
 
         [SlashCommand("tables", "View tables in this server")]
         public async Task Tables()
@@ -35,7 +40,9 @@ namespace CasinoBot.Interaction.Discord.Client.Modules
             }
 
             var embed = new EmbedBuilder()
-                .WithTitle("Tables 1");
+                .WithTitle("Tables");
+
+            var components = new ComponentBuilder();
 
             var tables = getTablesResponse.Value;
             if (tables is null || !tables.Any())
@@ -44,17 +51,29 @@ namespace CasinoBot.Interaction.Discord.Client.Modules
             }
             else
             {
-                // TODO: add in context buttons for navigating available tables
-                // Also TODO: learn how context buttons work
-            }
+                var table = tables.First();
+                var count = tables.Count();
+                embed.WithDescription($"{table.TableType}");
 
-            var components = new ComponentBuilder()
-                .WithButton("test", "testId:test");
+                if (count > 1)
+                {
+                    // TODO: emotes instead of text
+                    components.WithButton(emote: _nextButton, customId: $"nextTable:{table.TableId}");
+                    components.WithButton(emote: _prevButton, customId: $"prevTable:{table.TableId}");
+
+                }
+            }
+            components.WithButton(emote: _prevButton, customId: $"prevTable:{1}");
+            components.WithButton(emote: _nextButton, customId: $"nextTable:{1}");
 
             await FollowupAsync(components: components.Build(), embed: embed.Build());
         }
 
-        [ComponentInteraction("testId:*")]
+        #endregion
+
+        #region Components
+
+        [ComponentInteraction("nextTable:*")]
         public async Task NextTable()
         {
             if (Context.Interaction is SocketMessageComponent socketMessage)
@@ -73,6 +92,28 @@ namespace CasinoBot.Interaction.Discord.Client.Modules
                 await _loggingService.LogErrorMessage($"Unkown interaction type {Context.Interaction.GetType().Name} encountered in {nameof(NextTable)}");
             }
         }
+
+        [ComponentInteraction("prevTable:*")]
+        public async Task PrevTable()
+        {
+            if (Context.Interaction is SocketMessageComponent socketMessage)
+            {
+                var message = socketMessage.Message;
+                var x = int.Parse(message.Embeds.First().Title.Split(' ').Last()) + 1;
+
+                var embed = new EmbedBuilder()
+                    .WithTitle($"Tables {x}");
+
+                await message.ModifyAsync(m => m.Embed = embed.Build());
+                await RespondAsync();
+            }
+            else
+            {
+                await _loggingService.LogErrorMessage($"Unkown interaction type {Context.Interaction.GetType().Name} encountered in {nameof(NextTable)}");
+            }
+        }
+
+        #endregion
 
     }
 }
