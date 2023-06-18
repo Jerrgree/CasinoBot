dfa641b16109da062f853ba268db1822a5a732a2
Jerrgree <jeremyrgreenburg@gmail.com>
2023-06-18 17:34:56 -0500

    Add state variables

--- Games.BlackJack/Domain/Models/BlackJack.cs
+++ Games.BlackJack/Domain/Models/BlackJack.cs
@@ -7,9 +7,17 @@ namespace CasinoBot.Games.BlackJack.Domain.Models
     public class BlackJack : IGame
     {
         private Table _table;
+        private readonly IGameDataStore _gameDataStore;
+
+        public BlackJack(IGameDataStore gameDataStore)
+        {
+            _table = new();
+            _gameDataStore = gameDataStore;
+        }
+
         public async Task Play(IEnumerable<ulong> players, ulong guild)
         {
-            Initialize(players, guild)
+            Initialize(players, guild);
         }
 
         private void Initialize(IEnumerable<ulong> players, ulong guild)
@@ -17,7 +25,7 @@ namespace CasinoBot.Games.BlackJack.Domain.Models
             _table = new Table()
             {
                 TableType = TableType.BlackJack,
-                PlayerIds = players.ToList()
+                PlayerIds = players
             };
         }
     }
